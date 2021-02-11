f455058f9582721c808ead24505ee2a64c9135d4
varshitha-punati <72751737+varshitha-punati@users.noreply.github.com>
2021-02-11 12:05:17 +0530

    innerClasses

--- comm/innerclasses/RegularInnerClasses.java
+++ comm/innerclasses/RegularInnerClasses.java
@@ -1,19 +1,23 @@
 package comm.innerclasses;
 
 public class RegularInnerClasses {
+	// show method in outer class
 	void show() {
 		int a=20;
 		int b=12;
 		System.out.println("outerClass method");
 	}
+	//creating innerClass
 	class InnerClass{
 		int a=1;
 		int b=2;
+		//innerclass method
 		void display() {
 			System.out.println("InnerClass method:"+a);
 		}
 		
 	}
+	//main method
 		public static void main(String[] args) {
 			
 			//Here we can access only the innerclass methods
